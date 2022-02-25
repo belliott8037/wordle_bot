38f5011e13d7faf759b0d537ea3082c17cf2487f
belliott8037 <benelliottm93@gmail.com>
2022-02-25 15:15:56 -0600

    Redid the horrible loop added some error checking.

--- index.ts
+++ index.ts
@@ -4,44 +4,69 @@
 import DiscordJS, { Base, Channel, Intents, Options, TextChannel, ThreadChannel } from 'discord.js'
 import dotenv from 'dotenv'
 import fs from 'fs';
-dotenv.config()
+function main(){
+    dotenv.config()
+    // This is stupid don't judge me
+    // Set the arbitrary day is the day that you are starting the bot on
+    let arbitrary_day = 250
+    // set the arbitrary diff is the difference between your arbitrary_day and the length of your list
+    let arbitrary_diff = 20
+    // I'm sorry there's a better way of doing this but I don't care.
 
-// read in file
-const words = (fs.readFileSync("./Test wordles.csv", {
-    encoding: "utf-8"
-}).split(","));
-// craft a queue
-const the_queue = [];
-for (var index in words){
-    the_queue.push(words[index]);
-}
-// grab the word of the day
-let todays_word = the_queue.shift();
-// update the file after stripping out WoD
-fs.writeFileSync("./Test wordles.csv", the_queue.toString());
+    // read in file
+    const words = (fs.readFileSync("./Test wordles.csv", {
+        encoding: "utf-8"
+    }).split(","));
+    if (words[0] === ""){
+        console.log("Out of words")
+        return;
+    }
+    // Let's grab the word of the day
+    let todays_word = words[0].substring(1,6)
 
-const client = new DiscordJS.Client({
-    intents:[
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES
-        // Intents.FLAGS.GUILD_MESSAGE_REACTIONS
-    ]
-})
-client.on('ready', () => {
-    //                                                             ID token of the thread
-    const archive_thread: ThreadChannel = client.channels.cache.get("944764856672862269") as ThreadChannel;
-    console.log("The bot is ready")   
-    if(!archive_thread.isThread()){
-        console.log("Channel is not a thread")
-        return
+    // Let's do some stupid math
+    if (words.length < arbitrary_day){
+        var modifier = words.length + arbitrary_diff
     }
-    // post WoD
-    else if (typeof todays_word === "string"){
-        archive_thread.send(todays_word)
+    else{
+        var modifier = words.length - arbitrary_diff 
     }
-    else
-        console.log("Error bad word, or something horrible")
-})
+    // Get the offset to add
+    let offset = arbitrary_day - modifier
+    // Calculate the number
+    let todays_number = arbitrary_day + offset
+    // delete word from the list
+    words.shift()
+    // update the file after stripping out WoD
+    fs.writeFileSync("./Test wordles.csv", words.toString());
+    // Grab the date. yeah i got it off stackoverflow, sue me. https://stackoverflow.com/a/4929629
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.toLocaleString('en-us', {month: 'long'})).padStart(2, '0'); // Jan is apparently zero real smart.
+    var yyyy = today.getFullYear()
+    const client = new DiscordJS.Client({
+        intents:[
+            Intents.FLAGS.GUILDS,
+            Intents.FLAGS.GUILD_MESSAGES
+            // Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+        ]
+    })
+    client.on('ready', () => {
+        //                                                             ID token of the thread
+        const archive_thread: ThreadChannel = client.channels.cache.get("946857057322164264") as ThreadChannel;
+        console.log("The bot is ready")   
+        if(!archive_thread.isThread()){
+            console.log("Channel is not a thread")
+            return
+        }
+        // post WoD
+        else if (typeof todays_word === "string"){
+            // dude typescript is so stupid you have to use tic marks ` to display variables in the strings
+            archive_thread.send(`${mm} ${dd} ${yyyy}\nWordle #${todays_number}\n||${todays_word}||`)
+        }
+        else
+            console.log("Error bad word, or something horrible")
+    })
 // This is just messing around with a certain message "ping" is found in a channel then replys/reacts
 // client.on("messageCreate", (message) => {
 //     const archive_thread: TextChannel = client.channels.cache.get("944764856672862269") as TextChannel;
@@ -57,4 +82,6 @@ client.on('ready', () => {
 // }
 // )
 // TOKEN name needs to match the variable name you chose in the .env file
-client.login(process.env.TOKEN)
+    client.login(process.env.TOKEN)
+}
+main();
